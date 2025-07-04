56e0e572c3f180e9e15cc437502d2f8bd092caf7
SpadeMSI-Ubuntu <magic.global.k@gmail.com>
2025-07-04 05:26:17 -0400

    add motor-publisher.cpp

--- src/robovision_ros2/robovision_2/src/motor-publisher.cpp
+++ src/robovision_ros2/robovision_2/src/motor-publisher.cpp
@@ -1,7 +1,30 @@
 #include "rclcpp/rclcpp.hpp"
 #include "geometry_msgs/msg/twist.hpp"
 
-class TwistPublisher : public rclcpp::Node{
-    publisher
-    sdjkfh
+class MotorPublisher : public rclcpp::Node{
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
+    rclcpp::TimerBase::SharedPtr timer_;
+
+    public:
+    MotorPublisher() : Node("motor_publisher"){
+        publisher_ = this->create_publisher<geometry_msgs::msg::Twist>("/tid_kachaka/manual_control/cmd_vel", 10);
+        timer_ = this->create_wall_timer(std::chrono::milliseconds(100), std::bind(&TwistPublisher::timer_callback, this));
+    }
+
+    private:
+        void timer_callback(){
+            auto message = geometry_msgs::msg::Twist();
+            message.linear.x = 0.0;
+            message.angular.z = 0.0;
+            publisher_->publish(message);
+            RCLCPP_INFO(this->get_logger(), "Publishing: linear.x=%.2f, angular.z=%.2f", message.linear.x, message.angular.z);
+        }
+};
+
+int main(int argc, char *argv[]){
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<MotorPublisher>();
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
 }
