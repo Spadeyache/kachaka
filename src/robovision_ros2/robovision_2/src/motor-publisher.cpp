a45c0b42318fc41678393b0399986f8ad945f61e
SpadeMSI-Ubuntu <magic.global.k@gmail.com>
2025-07-07 03:48:41 -0400

    updated motor-pub: pubs gain based on delta_x

--- src/robovision_ros2/robovision_2/src/motor-publisher.cpp
+++ src/robovision_ros2/robovision_2/src/motor-publisher.cpp
@@ -1,5 +1,6 @@
 #include "rclcpp/rclcpp.hpp"
 #include "geometry_msgs/msg/twist.hpp"
+#include "std_msgs/msg/int32.hpp"
 
 using namespace std::chrono_literals;
 
@@ -11,6 +12,10 @@ public:
         publisher_ = this->create_publisher<geometry_msgs::msg::Twist>("/er_kachaka/manual_control/cmd_vel", 10);
         motion_timer_ = this->create_wall_timer(100ms, std::bind(&MotorPublisher::motion_callback, this));
 
+        // Subscriber to delta_x
+        delta_x_subscriber_ = this->create_subscription<std_msgs::msg::Int32>(
+            "/kn_delta", 10, std::bind(&MotorPublisher::callback_delta_x, this, std::placeholders::_1));
+
         // Timer 2: info logger every 1s
         info_timer_ = this->create_wall_timer(1.5s, [this]() {
             RCLCPP_INFO(this->get_logger(), "nodegit _loop");
@@ -21,25 +26,26 @@ public:
 private:
     void motion_callback()
     {
-        if(state_ % 2 == 0){
-            auto message = geometry_msgs::msg::Twist();
-            message.linear.x = 0.0;
+        auto message = geometry_msgs::msg::Twist();
+        if (delta_x_ > 0) {
             message.angular.z = 1.0;
-            publisher_->publish(message);
-            RCLCPP_INFO(this->get_logger(), "Publishing: linear.x=%.2f, angular.z=%.2f", message.linear.x, message.angular.z);
+        } else {
+            message.angular.z = -1.0;
         }
-        else{
-            auto message = geometry_msgs::msg::Twist();
-        message.linear.x = -3.0;
-        message.angular.z = 0.0;
         publisher_->publish(message);
-        RCLCPP_INFO(this->get_logger(), "Publishing: linear.x=%.2f, angular.z=%.2f", message.linear.x, message.angular.z);
-        }
-        
-
+        RCLCPP_INFO(this->get_logger(), "Publishing: angular.z=%.2f", message.angular.z);
     }
+
+    void callback_delta_x(const std_msgs::msg::Int32::SharedPtr msg)
+    {
+        delta_x_ = msg->data;
+        RCLCPP_INFO(this->get_logger(), "Received delta x: %d", delta_x_);
+    }
+
     int state_;
+    int delta_x_ = 0;
     rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
+    rclcpp::Subscription<std_msgs::msg::Int32>::SharedPtr delta_x_subscriber_;
     rclcpp::TimerBase::SharedPtr motion_timer_;
     rclcpp::TimerBase::SharedPtr info_timer_;
 };
