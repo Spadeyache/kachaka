07032541ed225093873ff6040327610103ebbf38
luis_contreras <tenshi.unam@gmail.com>
2025-07-07 13:08:25 +0900

    mtr controol with timer

--- src/robovision_ros2/robovision_2/src/motor-publisher.cpp
+++ src/robovision_ros2/robovision_2/src/motor-publisher.cpp
@@ -5,28 +5,40 @@ using namespace std::chrono_literals;
 
 class MotorPublisher : public rclcpp::Node {
 public:
-    MotorPublisher() : Node("motor_publisher")
+    MotorPublisher() : Node("motor_publisher"), state_(1)
     {
         // Timer 1: velocity publisher every 100ms
         publisher_ = this->create_publisher<geometry_msgs::msg::Twist>("/er_kachaka/manual_control/cmd_vel", 10);
         motion_timer_ = this->create_wall_timer(100ms, std::bind(&MotorPublisher::motion_callback, this));
 
         // Timer 2: info logger every 1s
-        info_timer_ = this->create_wall_timer(1s, [this]() {
+        info_timer_ = this->create_wall_timer(3s, [this]() {
             RCLCPP_INFO(this->get_logger(), "node_loop");
+            state_ += 1;
         });
     }
 
 private:
     void motion_callback()
     {
-        auto message = geometry_msgs::msg::Twist();
-        message.linear.x = -0.2;
+        if(state_ % 4 == 0){
+            auto message = geometry_msgs::msg::Twist();
+            message.linear.x = 1.6;
+            message.angular.z = 0.0;
+            publisher_->publish(message);
+            RCLCPP_INFO(this->get_logger(), "Publishing: linear.x=%.2f, angular.z=%.2f", message.linear.x, message.angular.z);
+        }
+        else{
+            auto message = geometry_msgs::msg::Twist();
+        message.linear.x = -1.6;
         message.angular.z = 0.0;
         publisher_->publish(message);
         RCLCPP_INFO(this->get_logger(), "Publishing: linear.x=%.2f, angular.z=%.2f", message.linear.x, message.angular.z);
-    }
+        }
+        
 
+    }
+    int state_;
     rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
     rclcpp::TimerBase::SharedPtr motion_timer_;
     rclcpp::TimerBase::SharedPtr info_timer_;
