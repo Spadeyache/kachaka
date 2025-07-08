3f9ade0d57dc44efa30f47efd845303da110dc78
SpadeMSI-Ubuntu <magic.global.k@gmail.com>
2025-07-08 02:32:15 -0400

    used syncronized

--- src/robovision_ros2/robovision_3/src/rgbd_pointClowd.cpp
+++ src/robovision_ros2/robovision_3/src/rgbd_pointClowd.cpp
@@ -4,79 +4,69 @@
 #include <cv_bridge/cv_bridge.h>
 #include <opencv2/opencv.hpp>
 #include <sensor_msgs/point_cloud2_iterator.hpp>
+#include <message_filters/subscriber.h>
+#include <message_filters/time_synchronizer.h>
+#include <message_filters/sync_policies/approximate_time.h>
 
 class RGBDPointCloudNode : public rclcpp::Node
 {
 public:
     RGBDPointCloudNode() : Node("rgbd_point_cloud_node")
     {
+        // Subscribers
+        rgb_sub_.subscribe(this, "/er_kachaka/front_camera/image_raw");
+        depth_sub_.subscribe(this, "/er_kachaka/tof_camera/image_raw");
 
-        //Subscribers
-        rclcpp::QoS qos(rclcpp::QoSInitialization::from_rmw(rmw_qos_profile_sensor_data));
-        qos.reliability(RMW_QOS_POLICY_RELIABILITY_BEST_EFFORT);
+        // Synchronizer
+        sync_.reset(new Sync(MySyncPolicy(10), rgb_sub_, depth_sub_));
+        sync_->registerCallback(std::bind(&RGBDPointCloudNode::callback, this, std::placeholders::_1, std::placeholders::_2));
 
-        rgb_sub_ = this->create_subscription<sensor_msgs::msg::Image>(
-            "/er_kachaka/front_camera/image_raw", qos, std::bind(&RGBDPointCloudNode::callback_rgb, this, std::placeholders::_1));
-        
-        depth_sub_ = this->create_subscription<sensor_msgs::msg::Image>(
-            "/er_kachaka/tof_camera/image_raw", qos, 
-            std::bind(&RGBDPointCloudNode::callback_depth, this, std::placeholders::_1));
-        
         // Publisher
-        point_cloud_pub_ = this->create_publisher<sensor_msgs::msg::PointCloud2>(
-            "/kn_point_cloud", 10);
+        point_cloud_pub_ = this->create_publisher<sensor_msgs::msg::PointCloud2>("/kn_point_cloud", 10);
 
         RCLCPP_INFO(this->get_logger(), "Starting rgbd_point_cloud_node application in cpp...");
     }
 
 private:
-    rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr rgb_sub_;
-    rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr depth_sub_;
+    message_filters::Subscriber<sensor_msgs::msg::Image> rgb_sub_;
+    message_filters::Subscriber<sensor_msgs::msg::Image> depth_sub_;
+    typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::msg::Image, sensor_msgs::msg::Image> MySyncPolicy;
+    std::shared_ptr<message_filters::Synchronizer<MySyncPolicy>> sync_;
+
     rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr point_cloud_pub_;
 
-    cv::Mat rgb_image_;
-    cv::Mat depth_image_;
-
-    void callback_rgb(const sensor_msgs::msg::Image::SharedPtr msg)
+    void callback(const sensor_msgs::msg::Image::SharedPtr rgb_msg, const sensor_msgs::msg::Image::SharedPtr depth_msg)
     {
         try
         {
-            cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvCopy(msg, msg->encoding);
-            rgb_image_ = cv_ptr->image.clone();
+            cv_bridge::CvImagePtr rgb_cv_ptr = cv_bridge::toCvCopy(rgb_msg, rgb_msg->encoding);
+            cv_bridge::CvImagePtr depth_cv_ptr = cv_bridge::toCvCopy(depth_msg, "32FC1");
+
+            cv::Mat rgb_image = rgb_cv_ptr->image;
+            cv::Mat depth_image = depth_cv_ptr->image;
+
+            if (rgb_image.empty() || depth_image.empty())
+            {
+                RCLCPP_WARN(this->get_logger(), "RGB or Depth image is empty.");
+                return;
+            }
+
+            // Compute point cloud
+            compute_point_cloud(rgb_image, depth_image);
         }
         catch (const std::exception &e)
         {
-            RCLCPP_ERROR(this->get_logger(), "Error processing RGB image: %s", e.what());
+            RCLCPP_ERROR(this->get_logger(), "Error processing images: %s", e.what());
         }
     }
 
-    void callback_depth(const sensor_msgs::msg::Image::SharedPtr msg)
+    void compute_point_cloud(const cv::Mat &rgb_image, const cv::Mat &depth_image)
     {
-        try
-        {
-            cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvCopy(msg, "32FC1");
-            depth_image_ = cv_ptr->image.clone();
-            compute_point_cloud();
-        }
-        catch (const std::exception &e)
-        {
-            RCLCPP_ERROR(this->get_logger(), "Error processing Depth image: %s", e.what());
-        }
-    }
-
-    void compute_point_cloud()
-    {
-        if (rgb_image_.empty() || depth_image_.empty())
-        {
-            RCLCPP_WARN(this->get_logger(), "RGB or Depth image is empty.");
-            return;
-        }
-
         // Create a PointCloud2 message
         sensor_msgs::msg::PointCloud2 point_cloud_msg;
         point_cloud_msg.header.frame_id = "camera_frame";
-        point_cloud_msg.height = depth_image_.rows;
-        point_cloud_msg.width = depth_image_.cols;
+        point_cloud_msg.height = depth_image.rows;
+        point_cloud_msg.width = depth_image.cols;
         point_cloud_msg.is_dense = false;
         point_cloud_msg.is_bigendian = false;
 
@@ -90,11 +80,11 @@ private:
         sensor_msgs::PointCloud2Iterator<uint8_t> iter_g(point_cloud_msg, "g");
         sensor_msgs::PointCloud2Iterator<uint8_t> iter_b(point_cloud_msg, "b");
 
-        for (int v = 0; v < depth_image_.rows; ++v)
+        for (int v = 0; v < depth_image.rows; ++v)
         {
-            for (int u = 0; u < depth_image_.cols; ++u, ++iter_x, ++iter_y, ++iter_z, ++iter_r, ++iter_g, ++iter_b)
+            for (int u = 0; u < depth_image.cols; ++u, ++iter_x, ++iter_y, ++iter_z, ++iter_r, ++iter_g, ++iter_b)
             {
-                float depth = depth_image_.at<float>(v, u);
+                float depth = depth_image.at<float>(v, u);
                 if (std::isnan(depth) || depth <= 0.0)
                 {
                     *iter_x = *iter_y = *iter_z = std::numeric_limits<float>::quiet_NaN();
@@ -107,7 +97,7 @@ private:
                     *iter_z = depth;
 
                     // Assign RGB values
-                    cv::Vec3b rgb = rgb_image_.at<cv::Vec3b>(v, u);
+                    cv::Vec3b rgb = rgb_image.at<cv::Vec3b>(v, u);
                     *iter_r = rgb[2]; // Red channel
                     *iter_g = rgb[1]; // Green channel
                     *iter_b = rgb[0]; // Blue channel
